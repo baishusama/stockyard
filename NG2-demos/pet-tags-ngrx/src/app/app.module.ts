e80ee4ac7a90b41df0b72423974d5c56cff7e0ed
baishusama <baishusama@outlook.com>
2017-07-14 11:56:11 +0800

    added NG2-demos/pet-tags-ngrx project

---
+++ NG2-demos/pet-tags-ngrx/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppRoutingModule } from './core/app-routing.module';
+
+import { AppComponent } from './core/app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CreateComponent } from './pages/create/create.component';
+import { CompleteComponent } from './pages/complete/complete.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CreateComponent,
+    CompleteComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
